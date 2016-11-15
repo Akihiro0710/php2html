b2dd494a9d91748aad8e6a70248eb26103045147
竹内　章裕 <TakeuchiAkihiro@aki.local>
2016-11-15 21:50:59 +0900

    ファイルの作成に成功

--- system/converter.php
+++ system/converter.php
@@ -1,6 +1,14 @@
 <?php
 require dirname(__FILE__)."/config.php";
 
-$str = file_get_contents($input_root."index.php");
-echo $str;
+$content = file_get_contents($input_root."index.php");
+echo $content;
+saveFile($output_root."index.html", $content);
+
+function saveFile($path, $content){
+  // make file
+  if(!file_exists($path)){
+    touch($path);
+  }
+}
 ?>
