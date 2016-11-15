e0894c41a63931b19cc1f2f3ba1812b9cfe453a7
竹内　章裕 <TakeuchiAkihiro@aki.local>
2016-11-15 22:31:53 +0900

    converterの実装を完了

--- system/converter.php
+++ system/converter.php
@@ -1,14 +1,31 @@
 <?php
-require dirname(__FILE__)."/config.php";
-
-$content = file_get_contents($input_root."index.php");
-echo $content;
-saveFile($output_root."index.html", $content);
+function convert($input, $output){
+  try{
+    $content = file_get_contents($input."?mode=convert");
+    saveFile($output, $content);
+  }catch(Exception $e){
+    throw new Exception("failed to convert file [$input]", $e->getCode(), $e);
+  }
+}
 
 function saveFile($path, $content){
-  // make file
-  if(!file_exists($path)){
-    touch($path);
+  $hundle = null;
+  try{
+    // make file
+    if(!file_exists($path)){
+      touch($path);
+    }
+    // change permission
+    chmod($path, 0666);
+    // file write
+    $handle = fopen($path, 'w');
+    fwrite($handle, $content);
+    fclose($handle);
+  }catch(ErrorException $e){
+    if(!is_null($handle)){
+      fclose($handle);
+    }
+    throw $e;
   }
 }
 ?>
